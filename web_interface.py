28ef7bc6d577f068e3917c15e2fb9ff1885535a7
Jackson Sims <jackson_d_sims@oa.eop.gov>
2018-03-20 21:57:43 -0400

    switch route to get and post

--- web_interface.py
+++ web_interface.py
@@ -12,20 +12,20 @@ import json
 
 app = application = default_app()
 
-@route('/refreshtool/oncompleterefresh/<org_name>/<server>')
+@get('/refreshtool/oncompleterefresh/<org_name>/<server>')
 def on_complete_refresh(org_name, server):
     controller_.RefreshMethods.on_complete_refresh(org_name, server)
     response.status = 200
     return response
 
-@route('/refreshtool/getrefreshoptions')
+@get('/refreshtool/getrefreshoptions')
 def get_refresh_options():
     options = json.dumps(controller_.RefreshMethods.get_refresh_options_display())
     response.status = 200
     response.body = options
     return response
 
-@route('/refreshtool/runselected/<org_name>/<server>')
+@post('/refreshtool/runselected/<org_name>/<server>')
 def run_selected():
     print(request.body)
     steps = request.json
